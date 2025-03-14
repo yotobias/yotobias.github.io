84cdb94e1dcde17f35f5bcf391e2c3cfacb85748
Tobias Peeters <tobias.peeters@student.uva.nl>
2025-03-14 23:13:00 +0100

    Added tempi histogram and tempograms

--- compmus.R
+++ compmus.R
@@ -363,3 +363,148 @@ compmus_mfccs <- function(file, norm = "identity") {
     dplyr::mutate(time = 1024 * time / 44100)
 }
 
+compmus_energy_novelty <- function(file) {
+  rjson::fromJSON(file = file) |>
+    purrr::pluck("lowlevel", "loudness_ebu128", "short_term") |>
+    (\(v) v[2:length(v)] - v[1:(length(v) - 1)])() |>
+    pmax(0) |>
+    (\(v) tibble::tibble(t = 0.1 * (1:length(v)) + 0.1, novelty = v))()
+}
+
+compmus_spectral_novelty <- function(file, norm = "euclidean", distance = "euclidean") {
+  ## Supported functions
+
+  manhattan <- function(x, y) sum(abs(x - y))
+  euclidean <- function(x, y) sqrt(sum((x - y)^2))
+  chebyshev <- function(x, y) max(abs(x - y))
+  pearson <- function(x, y) 1 - cor(x, y)
+  cosine <- function(x, y) {
+    1 - sum(compmus_normalise(x, "euc") * compmus_normalise(y, "euc"))
+  }
+  angular <- function(x, y) 2 * acos(1 - cosine(x, y)) / pi
+  aitchison <- function(x, y) {
+    euclidean(compmus_normalise(x, "clr"), compmus_normalise(y, "clr"))
+  }
+
+  ## Method aliases
+
+  METHODS <-
+    list(
+      manhattan = manhattan,
+      cityblock = manhattan,
+      taxicab = manhattan,
+      L1 = manhattan,
+      totvar = manhattan,
+      euclidean = euclidean,
+      L2 = euclidean,
+      chebyshev = chebyshev,
+      maximum = chebyshev,
+      pearson = pearson,
+      correlation = pearson,
+      cosine = cosine,
+      angular = angular,
+      aitchison = aitchison
+    )
+
+  ## Function selection
+
+  if (!is.na(i <- pmatch(distance, names(METHODS)))) {
+    rjson::fromJSON(file = file) |>
+      purrr::pluck("lowlevel", "mfcc") |>
+      purrr::map(\(v) compmus_normalise(v, norm)) |>
+      (\(v) purrr::map2_dbl(v[2:length(v)], v[1:(length(v) - 1)], METHODS[[i]]))() |>
+      (\(v) tibble::tibble(t = 1024 * (1:length(v) - 0.5) / 44100, novelty = v))()
+  } else {
+    stop("The distance name is ambiguous or the method is unsupported.")
+  }
+}
+
+#' @importFrom magrittr %>%
+.sample_tempogram <- function(y, f_s, window_size, hop_size, window_function, cyclic, bpms) {
+  window <- window_function(window_size)
+  if (cyclic) {
+    bpm_octaves <- rep(bpms, 5)
+    bpms <- bpms %>%
+      tcrossprod(2^(-2:2)) %>%
+      as.vector()
+  } else {
+    bpm_octaves <- bpms
+  }
+  bases <-
+    exp(tcrossprod(-2 * pi * 1i * (bpms / 60) / f_s, 0:(window_size - 1)))
+  if (length(y) > window_size) {
+    starts <- seq(1, length(y) - window_size, by = hop_size)
+  } else {
+    starts <- 1
+  }
+  windowed <- matrix(0, window_size, length(starts))
+  for (n in 1:length(starts)) {
+    windowed[, n] <- window * y[starts[n]:(starts[n] + window_size - 1)]
+  }
+  (bases %*% windowed) %>%
+    abs() %>%
+    magrittr::raise_to_power(2) %>%
+    as.vector() %>%
+    tibble::tibble(
+      time = rep((starts + window_size / 2) / f_s, each = length(bpms)),
+      bpm = rep(bpm_octaves, times = length(starts)),
+      power = .
+    ) %>%
+    dplyr::group_by(time, bpm) %>%
+    dplyr::summarise(power = sum(power)) %>%
+    dplyr::mutate(power = power / max(power)) %>%
+    dplyr::ungroup()
+}
+
+#' Compute a tempogram from Essentia segment onsets
+#'
+#' Computes a Fourier-based tempogram based on onsets of Spotify segments.
+#' Returns a tibble with \code{time}, \code{bpm}, and \code{power} columns.
+#' Power is normalised to a max of 1 (Chebyshev norm) within each time point.
+#'
+#' @param track_analysis Spotify audio analysis as returned by
+#'   \code{\link{get_tidy_audio_analysis}}.
+#' @param window_size Window size in seconds (default 8).
+#' @param hop_size Hop size in seconds (default 1).
+#' @param cyclic Boolean stating whether the tempogram should be cyclic (default
+#'   not).
+#' @param bpms Vector of tempi in beats per minute to include in the tempogram
+#'   (default 30--200 for non-cyclic and 80--160 for cyclic, inclusive of all
+#'   integer tempi).
+#' @param window_function Window function for the Fourier analysis (default
+#'   Hamming).
+#'
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' get_tidy_audio_analysis("6PJasPKAzNLSOzxeAH33j2") %>%
+#'   tempogram(window_size = 4, hop_size = 2)
+compmus_tempogram <- function(file, window_size = 8, hop_size = 1, cyclic = FALSE, bpms = if (cyclic) 80:160 else 30:600, window_function = signal::hamming) {
+  onsets <-
+    rjson::fromJSON(file = file) |>
+    purrr::pluck("rhythm", "beats_position") |>
+    magrittr::multiply_by(44100) %>%
+    round()
+
+  confidence <-
+    rjson::fromJSON(file = file) |>
+    purrr::pluck("rhythm", "beats_loudness")
+  duration <-
+    rjson::fromJSON(file = file) |>
+    purrr::pluck("metadata", "audio_properties", "length")
+
+  novelty <- rep(0, 44100 * duration)
+  novelty[onsets + 1] <- confidence
+
+  .sample_tempogram(
+    novelty,
+    f_s = 44100,
+    window_size = 44100 * window_size,
+    hop_size = 44100 * hop_size,
+    cyclic = cyclic,
+    bpms = bpms,
+    window_function = window_function
+  )
+}
